579888fc57e343bff817f270cf2760c2eb9cce49
Charlene Vas <cvas@fosterandpartners.com>
2025-01-22 14:34:57 +0000

    Initial commit

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../../services/data.service';
+import { AsyncPipe } from '@angular/common';
+import { IUser } from '../../interfaces/users';
+
+@Component({
+  selector: 'app-users',
+  imports: [AsyncPipe],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.scss'
+})
+export class UsersComponent implements OnInit {
+
+  dataService = inject(DataService)
+  userList$: Observable<IUser[]> = new Observable<IUser[]>;
+
+  ngOnInit(): void {
+    this.userList$ = this.dataService.getUsers();
+  }
+}
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUser } from '../interfaces/users';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  http = inject(HttpClient)
+  constructor() { }
+
+  getUsers(): Observable<IUser[]>{
+    return this.http.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+  }
+}
