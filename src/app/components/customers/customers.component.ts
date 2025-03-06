27c01fda9e86bfa164de530bc0ba5cbd4ed41e68
Adeola Boye <89581536+adeoladev@users.noreply.github.com>
2025-03-06 17:54:23 +0000

    Update customers.component.ts

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -4,7 +4,7 @@ import { ICustomer } from '../../interfaces/customer';
 import { DataService } from '../../services/data.service';
 import { AsyncPipe, NgFor } from '@angular/common';
 import { CommonModule } from '@angular/common';  
-import { IEmployee } from '../../interfaces/Employee';
+//import { IEmployee } from '../../interfaces/Employee';
 
 @Component({
   selector: 'app-customers',
@@ -17,7 +17,7 @@ export class CustomersComponent {
   dataService = inject(DataService)
 
   customerList$: Observable<ICustomer[]> = new Observable<ICustomer[]>;
-  employeeList$: Observable<IEmployee[]> = new Observable<IEmployee[]>;
+  //employeeList$: Observable<IEmployee[]> = new Observable<IEmployee[]>;
 
   employees: any;
 
